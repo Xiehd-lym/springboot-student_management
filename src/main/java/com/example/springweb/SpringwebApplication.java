db1ac07fce8a510d3de47b7cff1108dcdd713317
‘Xiehd-lym’ <xiehdwork@163.com>
2023-07-01 22:33:34 +0800

    Initial commit

---
+++ src/main/java/com/example/springweb/SpringwebApplication.java
@@ -0,0 +1,17 @@
+package com.example.springweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * http://localhost:8014/
+ * admin@163.com/123456
+ */
+@SpringBootApplication
+public class SpringwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringwebApplication.class, args);
+    }
+
+}
